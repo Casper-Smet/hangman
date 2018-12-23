f946d9d80833975a95f509f0923be7e1d79d634d
Casper Smet <casper.smet@student.hu.nl>
2018-12-23 23:50:14 +0100

    Made 'dcused.txt' for a functioning dictionary. Updated script.py to have a vs. computer mode.

--- script.py
+++ script.py
@@ -1,21 +1,29 @@
 import numpy as np
+import csv
+import random
 
-global word_list
-global guessing_list
-#maardananders
+
+def read_dictionary():
+    with open(r"dcused.txt", "r") as dictionary:
+        reader = csv.reader(dictionary)
+        dictionary_list = list()
+
+        for row in reader:
+            dictionary_list.append(row)
+    return dictionary_list
 
 
 def string_getter():
     word = str()
     while not word:
         word = str(input("Which word would you like to play? "))
-        if ' ' in word:
+        if not word.isalpha():
             word = str()
     word = word.upper()
     return list(word)
 
 
-def list_maker():
+def list_maker(word_list):
     guessing_list = list()
     for i in range(len(word_list)):
         guessing_list.append('_')
@@ -23,12 +31,12 @@ def list_maker():
     return guessing_list
 
 
-def replacer(locations):
+def replacer(locations, guessing_list, word_list):
     for i in locations:
         guessing_list[i] = word_list[i]
 
 
-def list_checker():
+def list_checker(word_list, guessing_list):
     win = lose = False
     values = np.array(word_list)
     game_count = 8
@@ -42,7 +50,7 @@ def list_checker():
         letter = letter.upper()
         if letter in word_list:
             locations = np.where(values == letter)[0]
-            replacer(locations)
+            replacer(locations, guessing_list, word_list)
         else:
             game_count += -1
         if '_' not in guessing_list:
@@ -51,11 +59,40 @@ def list_checker():
             lose = True
         print(guessing_list)
         print('You have said the following letters: {}'.format(guesses))
-        print('You have got false {} guesses left'.format(game_count))
+        print('You have got {} guesses left'.format(game_count))
     if win:
         print('Congratulations! You have won. The word was: {}'.format(str(word_list)))
+    else:
+        print('The word was: {}'.format(str(word_list)))
 
 
-word_list = string_getter()
-guessing_list = list_maker()
-list_checker()
+def computer():
+    dict = read_dictionary()
+    #print(len(dict))
+    range = len(dict)
+    word = dict[random.randrange(range)]
+    #print(word)
+    word_list = list(word[0].upper())
+    standard(word_list)
+
+def human():
+    word_list = string_getter()
+    standard(word_list)
+
+def standard(word_list):
+    #print(word_list)
+    guessing_list = list_maker(word_list)
+    #print(guessing_list)
+    list_checker(word_list, guessing_list)
+
+while True:
+    game_mode = str(input("Would you like to play against the computer or against another human?: "))
+    game_mode = game_mode.upper()
+    if game_mode == 'COMPUTER':
+        computer()
+    elif game_mode == 'HUMAN':
+        human()
+    else:
+        continue
+
+
